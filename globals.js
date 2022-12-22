ce378217111357d508f997bbad09b5d90b72abc9
Valery Smirnov <57757211+XantreGodlike@users.noreply.github.com>
2022-12-22 19:28:37 +0300

    Crash because reading process
    Crash because some package want to read some default field, so it need to rewrite some fields

--- globals.js
+++ globals.js
@@ -1,5 +1,29 @@
-global.Buffer = require('buffer').Buffer;
-global.process = require('process');
+/* eslint-disable no-undef */
+if (typeof __dirname === 'undefined') {
+  global.__dirname = '/';
+}
+if (typeof __filename === 'undefined') {
+  global.__filename = '';
+}
+
+if (typeof process === 'undefined') {
+  global.process = require('process');
+} else {
+  const bProcess = require('process');
+  for (var p in bProcess) {
+    if (!(p in process)) {
+      process[p] = bProcess[p];
+    }
+  }
+}
+
+process.browser = false;
+if (typeof Buffer === 'undefined') {
+  global.Buffer = require('buffer').Buffer;
+}
+
+const isDev = typeof __DEV__ === 'boolean' && __DEV__;
+process.env.NODE_ENV = isDev ? 'development' : 'production';
 
 // Needed so that 'stream-http' chooses the right default protocol.
 global.location = {
